1383da1a70335f4a9af5301c5a720dd136eb24bd
AndreiKirV <rjvveyyf@gmail.com>
2022-06-06 19:42:00 +0300

    map

--- Hunter/Assets/Scripts/World/GeneratorLevel.cs
+++ Hunter/Assets/Scripts/World/GeneratorLevel.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class GeneratorLevel : MonoBehaviour
 {
-    
+    public static int[,] GenerateArray(int width, int height, bool empty)
+    {
+        int[,] map = new int[width, height];
+
+        for (int x = 0; x < map.GetUpperBound(0); x++)
+        {
+            for (int y = 0; y < map.GetUpperBound(1); y++)
+            {
+                if (empty)
+                {
+                    map[x, y] = 0;
+                }
+                else
+                {
+                    map[x, y] = 1;
+                }
+            }
+        }
+        return map;
+    }
 }
